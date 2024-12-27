9abea8e41eedd3df64a235b6d9446078139fd3fb
Sudipta Das <shiningsudipto@gmail.com>
2024-12-27 19:44:32 +0600

    env and route middleware modified

--- src/app/modules/project/project.routes.ts
+++ src/app/modules/project/project.routes.ts
@@ -1,9 +1,16 @@
 import express from 'express'
 import { projectControllers } from './project.controller'
+import { parseBody } from '../../middlewares/bodyParser'
+import { multerUpload } from '../../config/multer.config'
 
 const router = express.Router()
 
-router.post('/', projectControllers.createProject)
+router.post(
+  '/',
+  multerUpload.single('img'),
+  parseBody,
+  projectControllers.createProject,
+)
 router.put('/', projectControllers.updateProject)
 router.get('/', projectControllers.getAllProject)
 router.get('/:slug', projectControllers.getSingleProject)
